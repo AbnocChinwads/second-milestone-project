771f9ec100631e74235ae0afbb8d0df77db46724
AbnocChinwads <rnemo@hotmail.co.uk>
2020-04-03 21:59:45 +0000

    Added buttons.js to control buttons. Changed styling to make buttons visible in both dark and light mode

---
+++ assets/buttons.js
@@ -0,0 +1,12 @@
+$("#button1").on("click", function() {
+    $("#button1").toggleClass("btn-danger");
+})
+$("#button2").on("click", function() {
+    $("#button2").toggleClass("btn-primary");
+})
+$("#button3").on("click", function() {
+    $("#button3").toggleClass("btn-success");
+})
+$("#button4").on("click", function() {
+    $("#button4").toggleClass("btn-warning");
+})
