11e74e4266cb6a31a537002742fbd1b4e8b75737
AbnocChinwads <rnemo@hotmail.co.uk>
2020-04-15 10:58:02 +0000

    Added level counter (only works to level 5)

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,32 +1,44 @@
 var score = 0;
+var level = 0;
 
 $("#score").html(`${score}`);
+$("#level").html(`${level}`);
 
 $("#start-button").on("click", function() {
     var buttons = document.getElementsByClassName("js-button");
     var buttonsToClick = chooseRandomButtons(buttons);
     currentButtons = buttonsToClick;
     flashButtons(buttonsToClick, 0);
+    level += 1;
+    $("#level").html(`${level}`);
 
   var currentOrder = 0;
   $(".js-button").off("click").on("click", function() {
     var selectedButton = $(this)[0];
     var button = currentButtons[0];
-    if (selectedButton === button.length - 1) {
-        alert("Well done! Press start again to begin the new round!");
-        score += 1;
-        $("#score").html(`${score}`);
-    } else if (selectedButton === button) {
+    if (selectedButton === button) {
         currentButtons.splice(button,1);
         score += 1;
         $("#score").html(`${score}`);
+        if (score == 4) {
+            alert("Well done! Click start to begin the next level");
+        } else if (score == 8) {
+            alert("Well done! Click start to begin the next level");
+        } else if (score == 13) {
+            alert("Well done! Click start to begin the next level");
+        } else if (score == 18) {
+            alert("Well done! Click start to begin the next level");
+        }
     } else {
         currentButtons = buttonsToClick;
         alert("Sorry, that was wrong. Click 'Start' to try again");
         score = 0;
+        level = 0;
         $("#score").html(`${score}`);
+        $("#level").html(`${level}`);
     }
   });
+
 })
 
 function chooseRandomButtons(buttons) {
